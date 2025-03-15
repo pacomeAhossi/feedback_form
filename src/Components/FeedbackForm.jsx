fd0be2b526184cedf1c6bea73c1b77c444de1aa9
Pacome AHOSSI <pacomeahossi175@gmail.com>
2025-03-15 10:37:28 -0400

    Create basic form template

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -5,14 +5,17 @@ const FeedbackForm = () => {
 
   return (
     <>
-    <nav>
-    Tell Us What You Think
-    </nav>
-      <form className="feedback-form">
-        <h2>We'd Love to Hear From You!</h2>
-        <p>Please share your feedback with us.</p>
-        
-      </form>
+        <nav>
+            Tell Us What You Think
+        </nav>
+        <form className="feedback-form">
+            <h2>We'd Love to Hear From You!</h2>
+            <p>Please share your feedback with us.</p>
+            <input type="text" name='name' placeholder='Your Name' />
+            <input type="email" name='email' placeholder='Your Email' />
+            <textarea name="feedback" cols="30" rows="10" placeholder='Your Feedback'></textarea>
+            <button type='submit'>Submit feedback</button>
+        </form>
     </>
   );
 };
